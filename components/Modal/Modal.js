b5fe2818feb35883ee7d1b5f3c913bc3bfbc1b45
Aleksandr Ufimtsev <fimak@bk.ru>
2022-01-12 22:48:52 +0600

    overlay fix

--- components/Modal/Modal.js
+++ components/Modal/Modal.js
@@ -1,11 +1,21 @@
+import { useEffect } from 'react';
 import Overlay from './Overlay';
 import styles from './Modal.module.sass';
 
 export default function Modal({
                                 open = false,
-                                setOpen = () => {},
+                                setOpen = () => {
+                                },
                                 children,
                               }) {
+  useEffect(() => {
+    if (open) {
+      document.body.classList.add("no-scroll");
+    } else {
+      document.body.classList.remove("no-scroll");
+    }
+  }, [open]);
+
   return open && (
     <Overlay onClick={() => setOpen(false)}>
       <div className={styles.root}>
